19b19026b79d1c2eac03744d783e05a9d3befcce
if17 <if17@A406-18.INFORMAATIKA>
2018-03-08 08:55:00 +0200

    Springi korrutus

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -7,15 +7,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class Rakendus {
-	@RequestMapping("/algus")
+    @RequestMapping("/algus")
     String tervitusfunktsioon() {
         return "Ahoi!";
     }
     
     @RequestMapping("/tervitus")
     String tervitus2(String eesnimi){
-        return "Tere, "+eesnimi;
+        return "Tere, "+ eesnimi;
     }
+    
+    @RequestMapping("/korrutus")
+    int korrutamine(int arv1, int arv2) {
+        return arv1 * arv2;
+    } //localhost:8080/korrutus&arv1=3&arv2=6/
  
     public static void main(String[] args) {
 		//System.getProperties().put("server.port", 40305);
